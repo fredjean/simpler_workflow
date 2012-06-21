629183240b0bc9ccf3e121323feabe43d8177a62
Frederic Jean <fred@fredjean.net>
2012-06-21 11:54:01 -0600

    Stubbing the list_domains response.

--- spec/simpler_workflow_spec.rb
+++ spec/simpler_workflow_spec.rb
@@ -4,7 +4,8 @@ describe SimplerWorkflow do
 
   let(:client) { AWS.config.simple_workflow_client }
 
-  let(:response) { client.stub_for(:list_domains) }
+  let(:describe_domain_response) { client.stub_for(:describe_domain) }
+  let(:list_domains_response) {client.stub_for(:list_domains)}
 
   let(:domain) { SimplerWorkflow.domain("test-domain") }
 
@@ -17,22 +18,33 @@ describe SimplerWorkflow do
     },
   }}
 
-  before :each do
-    response.stub(:data).and_return(domain_desc)
-    client.stub(:describe_domain).and_return(response)
-  end
+  let(:domains_desc) {
+    {
+      'domainInfos' => [
+        domain_desc['domainInfo']
+      ]
+    }
+  }
 
-  it "should return the domain for the test" do
-    domain.should_not be_nil
-    domain.should be_a(SimplerWorkflow::Domain)
-  end
+  context "Referring to a domain" do
+    before :each do
+      describe_domain_response.stub(:data).and_return(domain_desc)
+      client.stub(:describe_domain).and_return(describe_domain_response)
+      list_domains_response.stub(:data).and_return(domains_desc)
+      client.stub(:list_domains).and_return(list_domains_response)
+    end
 
-  it "should provide the domain name" do
-    domain.name.should == "test-domain"
-  end
+    it "should return the domain for the test" do
+      domain.should_not be_nil
+      domain.should be_a(SimplerWorkflow::Domain)
+    end
 
-  it "should show the default retention period" do
-    pending "Need to figure out how to stub this correctly."
-    domain.retention_period.should == 2
+    it "should provide the domain name" do
+      domain.name.should == "test-domain"
+    end
+
+    it "should show the default retention period" do
+      domain.retention_period.should == 2
+    end
   end
 end
