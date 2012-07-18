99a95497dbd8744c15055f7223d8704ca9751077
Frederic Jean <fred@fredjean.net>
2012-07-17 23:56:11 -0600

    Memoizing the events instead a single event.

--- lib/aws/simple_workflow/decision_task_additions.rb
+++ lib/aws/simple_workflow/decision_task_additions.rb
@@ -3,8 +3,17 @@ require 'aws/simple_workflow/decision_task'
 module AWS
   class SimpleWorkflow
     module DecisionTaskAdditions
+      def self.included(base)
+        base.__send__ :alias_method, :_original_events, :_events
+        base.__send__ :protected, :_original_events
+      end
+
       def scheduled_event(event)
         events.to_a[event.attributes.scheduled_event_id - 1]
+      end
+
+      def _events &block
+        @_events ||= _original_event &block
       end
     end
 
