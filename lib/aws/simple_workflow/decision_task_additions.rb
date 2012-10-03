e1ff7952d01e387385b7c356834caac161f90470
Frederic Jean <fred@fredjean.net>
2012-10-03 11:31:47 -0600

    Experiment to see whether we can get events without having to pull the whole list from Amazon.

--- lib/aws/simple_workflow/decision_task_additions.rb
+++ lib/aws/simple_workflow/decision_task_additions.rb
@@ -3,20 +3,10 @@ require 'aws/simple_workflow/decision_task'
 module AWS
   class SimpleWorkflow
     module DecisionTaskAdditions
-      def self.extended(inst)
-        inst.class.__send__ :alias_method, :_original_events, :events
-      end
-
       def scheduled_event(event)
-        events.to_a[event.attributes.scheduled_event_id - 1]
+        events.reverse_order.find { |e| e.id == event.attributes.scheduled_event_id }
       end
-
-      def events
-        @__original_events ||= _original_events
-      end
-
     end
-
   end
 end
 
