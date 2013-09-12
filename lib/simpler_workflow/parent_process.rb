19a0599ffc4bc6fcc0e8c4369173b9b58416f5d6
David Pelaez <pelaez89@gmail.com>
2013-09-11 20:56:24 -0500

    Updating parent

--- lib/simpler_workflow/parent_process.rb
+++ lib/simpler_workflow/parent_process.rb
@@ -8,16 +8,15 @@ module SimplerWorkflow
 			
 			base.extend ClassMethods
 
+
+
 			# Define some defaults. 
 			# They can be overwritten using the convenience methods
-			base.logfile_path "log/#{base.to_s.downcase}.log"
+			#base.logfile_path "log/#{base.to_s.downcase}.log"
 			# put the ephimeral data in pid folder
-			base.pidfile_path "log/pid/#{base.to_s.downcase}.pid"
+			#base.pidfile_path "log/pid/#{base.to_s.downcase}.pid"
 
-			# By using USR1 instead of the QUIT signal, we ensure that
-			# this process will only close if all it's children are terminated
-			# this convention avoids zombie processes.
-			Signal.trap('USR1') { base.graceful_exit 'QUIT'}
+			Signal.trap('TERM') { base.graceful_exit 'QUIT'}
 
 			Signal.trap('INT') { base.graceful_exit 'INT'}
 			
@@ -40,17 +39,17 @@ module SimplerWorkflow
 				@workers = val
 			end
 
-			def pidfile_path(val)
-				@pidfile_path = val
-			end
-
-			def lockfile_path(val)
-				@lockfile_path = val
-			end
-
-			def logfile_path(val)
-				@logfile_path = val
-			end
+			#def pidfile_path(val)
+			#	@pidfile_path = val
+			#end
+#
+			#def lockfile_path(val)
+			#	@lockfile_path = val
+			#end
+#
+			#def logfile_path(val)
+			#	@logfile_path = val
+			#end
 
 			def log_level(val)
 				@log_level = val
@@ -61,26 +60,27 @@ module SimplerWorkflow
 			# elements from the rake tasks and bin/swf
 			def on_boot(&block)
 				
-				pid = Integer(IO.read(@pidfile_path).strip) rescue nil
-				running = begin; Process.kill(0, pid); true; rescue Object; false; end
-				if running
+				#pid = Integer(IO.read(@pidfile_path).strip) rescue nil
+				#running = begin; Process.kill(0, pid); true; rescue Object; false; end
+				#if running
 					# custom exit code captured in the rake task
-					exit 42
-				else
-					File.open(@pidfile_path, "w") { |f| f.write(Process.pid) }
-				
+				#	exit 42
+				#else
+					#Process.daemon
+					#File.open(@pidfile_path, "w") { |f| f.write(Process.pid) }
 					# If we're ready to run, prepare the logger
-					$logger = Logger.new(@logfile_path)
+					#$logger = Logger.new(@logfile_path)
+					$logger = Logger.new STDOUT
 					$logger.level = @log_level if @log_level
 					# separate this execution in the log
 					$logger.info "Booting with #{@workers} workers----------------------------"
 					@workers.times { yield }
 					# we wait for all children processes to exit; when QUIT is sent
 					# we terminate them and this will automatically exit.
-					Process.setsid
-					Process.daemon
+					#Process.setsid
+					#puts "!!!!"
 					Process.waitall
-				end
+				#end
 			end
 
 		end
